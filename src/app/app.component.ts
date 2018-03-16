90df18341d63ca734ce6d6f7ab4032f791983946
lulin <lulin@gridsum.com>
2018-03-16 12:47:28 +0800

    up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+  list: string[] = [];
+  scrollTop: number;
+
+  constructor() {
+    this.scrollTop = 0;
+  }
+
+  ngOnInit() {
+    const item = '小撸家的猫';
+
+    for (let i = 0; i < 100; i++) {
+      this.list.push(item);
+    }
+  }
+
+  onScrollChange(el: Element) {
+    this.scrollTop = el.scrollTop;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { ScrollDirective } from './scroll/scroll.directive';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ScrollDirective
   ],
   imports: [
     BrowserModule
